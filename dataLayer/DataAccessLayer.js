c1407ca406981f84704267195e642799a166064e
RSamokhin <rsamokhin@rsamokhin-mac-15-old.local>
2018-08-26 15:50:18 +0300

    rename servic

--- dataLayer/DataAccessLayer.js
+++ dataLayer/DataAccessLayer.js
@@ -14,10 +14,11 @@ DataAccessLayer.generateSequence = function (seqName, seqKeys, seqValues) {
         })
     });
     return {
-        name: seqName,
+        key: seqName,
         data: data
     }
 }
+
 DataAccessLayer.getTotalDailyItemsSold = function () {
     return provideAllData().then((data) => {
         const keys = Array.from(new Set(data.map(deal => deal.date)));
@@ -29,4 +30,31 @@ DataAccessLayer.getTotalDailyItemsSold = function () {
     });
 }
 
+// DataAccessLayer.getAverageDailyPuddingItemsPerCustomer = function () {
+//     return provideAllData().then((data) => {
+//         const keys = Array.from(new Set(data.map(deal => deal.date)));
+//         const vaues = keys.map(date => data.filter(deal => deal.date === date).length);
+        
+//         return [
+//             DataAccessLayer.generateSequence('Total Daily Items Sold', keys, vaues)
+//         ];
+//     });
+// }
+
+
+
+//
+
+// getTotalDailyItemsSold: createApiService('total_daily_items_sold'),
+//         getAverageDailyPuddingItemsPerCustomer: createApiService('average_daily_pudding_items_per_customer'),
+//         getTotalDailyItemSalesPerPudding: createApiService('total_daily_item_sales_per_pudding')
+
+// /api/total_daily_item_sales_per_pudding
+
+// 1) Total daily pudding items sold
+// 2) Average daily pudding items sold per customer
+// 3) Total daily item sales, split into a line for each pudding item (Dani, Milki etc)
+
+//customerId,item,count,date
+// 2993,Dani,4,02/06/2016
 module.exports = DataAccessLayer;
--- routes/api.js
+++ routes/api.js
@@ -4,7 +4,7 @@ var {
     getTotalDailyItemsSold
 } = require('../dataLayer/DataAccessLayer');
 
-router.get('/example', function(req, res) {
+router.get('/api/total_daily_items_sold', function(req, res) {
     getTotalDailyItemsSold()
         .then(function(result) {
             res
@@ -22,13 +22,41 @@ router.get('/example', function(req, res) {
         });
 });
 
+router.get('/api/average_daily_pudding_items_per_customer', function(req, res) {
+    getTotalDailyItemsSold()
+        .then(function(result) {
+            res
+                .status(200)
+                .json(result);
+        }, function(err) {
+            res
+                .status(500)
+                .send(err);
+        })
+        .catch(err => {
+            res
+                .status(500)
+                .send(err);;
+        });
+});
 
+router.get('/api/total_daily_item_sales_per_pudding', function(req, res) {
+    getTotalDailyItemsSold()
+        .then(function(result) {
+            res
+                .status(200)
+                .json(result);
+        }, function(err) {
+            res
+                .status(500)
+                .send(err);
+        })
+        .catch(err => {
+            res
+                .status(500)
+                .send(err);;
+        });
+});
 
-// 1) Total daily pudding items sold
-// 2) Average daily pudding items sold per customer
-// 3) Total daily item sales, split into a line for each pudding item (Dani, Milki etc)
-
-//customerId,item,count,date
-// 2993,Dani,4,02/06/2016
 
 module.exports = router;
